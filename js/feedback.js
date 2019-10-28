a225f084fd6bba77f74d0decdf3aff6f37231c88
Manul228 <qawaii@yandex.ru>
2019-10-29 00:36:56 +0300

    щекотка

--- js/feedback.js
+++ js/feedback.js
@@ -22,6 +22,7 @@ form.addEventListener('submit', function(evt) {
   if (!username.value || !email.value || !message.value) {
     evt.preventDefault();
     popup.classList.remove('modal-error');
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add('modal-error');
   }
 });
